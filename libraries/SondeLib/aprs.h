9b0785bc44b1346d8e570f63df7b98469c1c0a3b
Hans P. Reiser <hr@sec.uni-passau.de>
2019-04-12 17:57:03 +0200

    +setup instructions

--- libraries/SondeLib/aprs.h
+++ libraries/SondeLib/aprs.h
@@ -2,6 +2,16 @@
 #ifndef _aprs_h
 #define _aprs_h
 
+enum IDTYPE { ID_DFMDXL, ID_DFMREAL, ID_DFMAUTO };
+
+typedef struct st_feedinfo {
+	bool active;
+	int type;	// 0:UDP(axudp), 1:TCP(aprs.fi)
+	char *host;
+	int port;
+	int rate;
+	int idformat;	// 0: dxl  1: real  2: auto
+}
 #define MAXLEN 201
 void aprs_gencrctab(void);
 int aprsstr_mon2raw(const char *mon, char raw[], int raw_len);
