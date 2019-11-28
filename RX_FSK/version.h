76840649b63e536280f98bee8a5be04dde853ce1
Hansi, dl9rdz <dl9rdz@darc.de>
2019-11-28 08:06:05 +0100

    batt info for oled

--- RX_FSK/version.h
+++ RX_FSK/version.h
@@ -1,4 +1,4 @@
 const char *version_name = "rdzTTGOsonde";
-const char *version_id = "devel20191125";
+const char *version_id = "devel20191128";
 const int SPIFFS_MAJOR=2;
 const int SPIFFS_MINOR=1;
