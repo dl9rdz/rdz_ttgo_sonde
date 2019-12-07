5459f76a7d94a0037004e6a64383b88b5e9363c6
Hansi, dl9rdz <dl9rdz@darc.de>
2019-12-07 17:14:19 +0100

    version update

--- RX_FSK/version.h
+++ RX_FSK/version.h
@@ -1,4 +1,4 @@
 const char *version_name = "rdzTTGOsonde";
-const char *version_id = "devel20191129";
+const char *version_id = "devel20191207";
 const int SPIFFS_MAJOR=2;
 const int SPIFFS_MINOR=1;
