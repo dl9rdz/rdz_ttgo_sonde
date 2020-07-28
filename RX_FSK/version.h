052f502d456356f423205941d4060fb671616e7b
Hansi, dl9rdz <dl9rdz@darc.de>
2020-07-28 22:12:16 +0200

    update version with cixio mod

--- RX_FSK/version.h
+++ RX_FSK/version.h
@@ -1,4 +1,4 @@
 const char *version_name = "rdzTTGOsonde";
-const char *version_id = "devel20200414";
+const char *version_id = "devel20200728";
 const int SPIFFS_MAJOR=2;
 const int SPIFFS_MINOR=4;
