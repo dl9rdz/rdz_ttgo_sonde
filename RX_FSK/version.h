1954cafa6f2d09c14db1938802929b6e243910e2
Hansi, dl9rdz <dl9rdz@darc.de>
2020-11-20 22:07:29 +0100

    support development with platformio (in addition to Arduino IDE)

--- RX_FSK/version.h
+++ RX_FSK/version.h
@@ -1,4 +1,4 @@
 const char *version_name = "rdzTTGOsonde";
-const char *version_id = "devel20201118";
+const char *version_id = "devel20201120";
 const int SPIFFS_MAJOR=2;
 const int SPIFFS_MINOR=4;
