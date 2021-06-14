8861458421aa05b028a497cbbaf0f9dec2e17b1c
Uskompuf <22492406+Uskompuf@users.noreply.github.com>
2021-06-14 11:55:26 +1000

    GPS Support

--- RX_FSK/version.h
+++ RX_FSK/version.h
@@ -1,4 +1,4 @@
 const char *version_name = "rdzTTGOsonde";
-const char *version_id = "devel20210531";
+const char *version_id = "devel20210614";
 const int SPIFFS_MAJOR=2;
 const int SPIFFS_MINOR=11;
