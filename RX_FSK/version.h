97b3abc38fb2f9987028790f6ff0526f2ff60ad6
Hansi, dl9rdz <dl9rdz@darc.de>
2021-10-09 10:11:03 +0200

    version update for dark mode enhancements

--- RX_FSK/version.h
+++ RX_FSK/version.h
@@ -1,4 +1,4 @@
 const char *version_name = "rdzTTGOsonde";
-const char *version_id = "devel20211005";
+const char *version_id = "devel20211009";
 const int SPIFFS_MAJOR=2;
 const int SPIFFS_MINOR=16;
