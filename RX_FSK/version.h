c63c31a1d4ad24f966f4c3642da0baa3a89de614
Hansi, dl9rdz <dl9rdz@darc.de>
2022-12-17 23:53:51 +0100

    fix GD5 (untested)

--- RX_FSK/version.h
+++ RX_FSK/version.h
@@ -1,4 +1,4 @@
 const char *version_name = "rdzTTGOsonde";
-const char *version_id = "devel20221113";
+const char *version_id = "devel20221217";
 const int SPIFFS_MAJOR=2;
 const int SPIFFS_MINOR=17;
