5fea623f7d9e5e5ed1793a8c120042a3cae95fd2
Hansi, dl9rdz <dl9rdz@darc.de>
2023-04-13 15:15:32 +0000

    python3 compatibility

--- scripts/makeimage.py
+++ scripts/makeimage.py
@@ -13,7 +13,7 @@ import subprocess
 #spiffs,   data, spiffs,  0x290000,0x170000,
 
 MKSPIFFS = os.environ['MKSPIFFS']
-print "mkspiffs is "+MKSPIFFS
+print("mkspiffs is "+MKSPIFFS)
 
 OFFSET_BOOTLOADER = 0x1000
 OFFSET_PARTITIONS = 0x8000
@@ -44,9 +44,9 @@ with open(partition, 'rb') as csvfile:
 			OFFSET_SPIFFS = int(row[3],16)
 			SIZE_SPIFFS = int(row[4],16)
 
-print "bootapp0: "+hex(OFFSET_BOOTAPP0)
-print "app0: "+hex(OFFSET_APPLICATION)
-print "spiffs: "+hex(OFFSET_SPIFFS)+" size "+hex(SIZE_SPIFFS)
+print("bootapp0: "+hex(OFFSET_BOOTAPP0))
+print("app0: "+hex(OFFSET_APPLICATION))
+print("spiffs: "+hex(OFFSET_SPIFFS)+" size "+hex(SIZE_SPIFFS))
 
 # create binary partition
 file_part = "/tmp/partition.bin"
